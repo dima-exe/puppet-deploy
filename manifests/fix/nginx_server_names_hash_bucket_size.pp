efda414ff519402df03af97348f309deac8f9c88
Dmitry Galinsky <dima.exe@gmail.com>
2013-09-27 01:13:22 +0400

    fixes error: Invalid relationship ..., because Service[nginx] doesn't seem to be in the catalog

--- manifests/fix/nginx_server_names_hash_bucket_size.pp
+++ manifests/fix/nginx_server_names_hash_bucket_size.pp
@@ -3,9 +3,11 @@ class deploy::fix::nginx_server_names_hash_bucket_size(
   $size = 64
 ) {
 
-  file { '/etc/nginx/conf.d/server_names_hash_bucket_size.conf':
-    content => "server_names_hash_bucket_size ${size};\n",
-    notify  => Service['nginx'],
-    require => Package['nginx']
+  if defined(Service['nginx']) {
+    file { '/etc/nginx/conf.d/server_names_hash_bucket_size.conf':
+      content => "server_names_hash_bucket_size ${size};\n",
+      notify  => Service['nginx'],
+      require => Package['nginx']
+    }
   }
 }
