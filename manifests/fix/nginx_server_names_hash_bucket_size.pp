75e1d2639e07233cdaf2411757e5c3b3c7ec0114
Dmitry Galinsky <dima.exe@gmail.com>
2013-10-02 21:11:47 +0400

    fixes deploy::fix::nginx_server_names_hash_bucket_size

--- manifests/fix/nginx_server_names_hash_bucket_size.pp
+++ manifests/fix/nginx_server_names_hash_bucket_size.pp
@@ -3,11 +3,9 @@ class deploy::fix::nginx_server_names_hash_bucket_size(
   $size = 64
 ) {
 
-  if defined(Service['nginx']) {
-    file { '/etc/nginx/conf.d/server_names_hash_bucket_size.conf':
-      content => "server_names_hash_bucket_size ${size};\n",
-      notify  => Service['nginx'],
-      require => Package['nginx']
-    }
+  file { '/etc/nginx/conf.d/server_names_hash_bucket_size.conf':
+    content => "server_names_hash_bucket_size ${size};\n",
+    notify  => Service['nginx'],
+    require => Package['nginx']
   }
 }
