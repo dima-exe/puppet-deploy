fbef4d530bb80f9c3efbf11d90abeb43392d3273
Dmitry Galinsky <dima.exe@gmail.com>
2013-11-13 22:04:52 +0400

    fixes specs

--- spec/unit/defines/deploy_rabbitmq_spec.rb
+++ spec/unit/defines/deploy_rabbitmq_spec.rb
@@ -4,13 +4,13 @@ describe "deploy::rabbitmq", :type => :define do
   let(:title) { 'my-user' }
   let(:default_params) { { 'vhost' => 'vhost', 'password' => 'pass' } }
   let(:params) { default_params }
-  let(:facts) { {osfamily: "Debian"} }
+  let(:facts) { {:osfamily => "Debian"} }
 
   it { should include_class("rabbitmq") }
 
 
   it do should contain_resource("Rabbitmq_vhost[vhost]").with(
-    :ensure => 'present',
+    :ensure => 'present'
   ) end
 
   it do should contain_resource("Rabbitmq_user[my-user]").with(
@@ -22,20 +22,20 @@ describe "deploy::rabbitmq", :type => :define do
   it do should contain_resource("Rabbitmq_user_permissions[my-user@vhost]").with(
     :configure_permission => ".*",
     :read_permission      => ".*",
-    :write_permission     => ".*",
+    :write_permission     => ".*"
   ) end
 
   context "when $ensure is absent" do
     let(:params) { default_params.merge :ensure => "absent" }
     it do should contain_resource("Rabbitmq_user[my-user]").with(
-      :ensure   => 'absent',
+      :ensure   => 'absent'
     ) end
   end
 
   context "when $admin is true" do
     let(:params) { default_params.merge :admin => true }
     it do should contain_resource("Rabbitmq_user[my-user]").with(
-      :admin   => true,
+      :admin   => true
     ) end
   end
 
